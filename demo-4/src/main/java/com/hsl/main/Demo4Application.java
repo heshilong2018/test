0b006892dd851f13282ed2bb606a42eb87b63005
heshilong <975957362@qq.com>
2018-08-07 11:39:50 +0800

    第一次

---
+++ demo-4/src/main/java/com/hsl/main/Demo4Application.java
@@ -0,0 +1,12 @@
+package com.hsl.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo4Application {
+//测试
+	public static void main(String[] args) {
+		SpringApplication.run(Demo4Application.class, args);
+	}
+}
